3f80d6ea6b3b85c1caef4a8ae522b719d05d79b0
YaredT21 <teklewold.yared21@gmail.com>
2022-05-26 12:36:48 +0300

    Update index.js

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,7 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import {
   faLinkedin,
   faGithub,
